9e11622fa849ab6f1e38763e75696a3d98c67f33
yuma14 <yuma140902@gmail.com>
2023-11-08 00:38:37 +0900

    PWA and OGP

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
 import { visualizer } from 'rollup-plugin-visualizer';
@@ -16,6 +17,54 @@ export default defineConfig(({ mode }) => {
     plugins: [
       react(),
       wasm(),
+      VitePWA({
+        includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+        registerType: 'prompt',
+        workbox: {
+          globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+          maximumFileSizeToCacheInBytes: 12 * 1024 * 1024,
+        },
+        strategies: 'generateSW',
+        manifest: {
+          background_color: '#94c0fa',
+          categories: ['graphics'],
+          display: 'fullscreen',
+          icons: [
+            {
+              src: '/regend-webui/192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: '/regend-webui/256.png',
+              sizes: '256x256',
+              type: 'image/png',
+            },
+            {
+              src: '/regend-webui/384.png',
+              sizes: '384x384',
+              type: 'image/png',
+            },
+            {
+              src: '/regend-webui/512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: '/regend-webui/512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any',
+            },
+          ],
+          name: 'Regend WebUI',
+          orientation: 'any',
+          scope: '/',
+          short_name: 'Regend',
+          start_url: '/regend-webui/',
+          theme_color: '#3884eb',
+        },
+      }),
       topLevelAwait({
         // The export name of top-level await promise for each chunk module
         promiseExportName: '__tla',
